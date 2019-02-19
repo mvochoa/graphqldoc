0bfa599a440c9fd4c3da274aafcf1b9de265548f
Mario Valentin Ochoa Mota <mvochoaa@gmail.com>
2019-02-19 13:37:15 -0600

    Added script for bin

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"gitlab.com/mvochoa/graphqldoc"
+)
+
+func main() {
+	if len(os.Args) > 1 {
+		u, err := url.ParseRequestURI(os.Args[1])
+		if err == nil {
+			fmt.Print("Generating documentation...\n")
+			graphqldoc.HTTP(u.String())
+			return
+		}
+
+		fmt.Println("ERROR: The endpoint is incorrect.")
+	}
+	fmt.Print("\n	Usage: graphdoc <ENDPOINT GRAPHQL>\n\n")
+}
